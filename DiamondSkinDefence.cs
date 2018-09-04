077696c9b26a248a863187e1ec370e59a40033bd
dbarraza <32948749+dbarraza@users.noreply.github.com>
2018-09-04 17:45:52 -0300

    Add files via upload

---
+++ DiamondSkinDefence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NullObjectPattern
+{
+    public class DiamondSkinDefence : SpecialDefence
+    {
+        public override int CaculateDamageReduction(int totalDamage)
+        {
+            return 1;
+        }
+    }
+}
---
+++ SpecialDefence.cs
@@ -0,0 +1,21 @@
+﻿namespace NullObjectPattern
+{
+    public abstract class SpecialDefence
+    {
+        public abstract int CaculateDamageReduction(int totalDamage);
+
+        //Singleton
+        public static SpecialDefence NullObject
+        {
+            get { return new NonDefence(); }
+        }
+
+        private class NonDefence : SpecialDefence
+        {
+            public override int CaculateDamageReduction(int totalDamage)
+            {
+                return 0; //*** no-op behavior
+            }
+        }
+    }
+}
